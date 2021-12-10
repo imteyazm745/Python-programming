73e6b05b340578c627141cc5ac62cea8c1953211
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-12-10 23:23:02 +0530

    Create count_digit.py

---
+++ Basic Python/count_digit.py
@@ -0,0 +1,7 @@
+#python program to count number of digits in a number
+n=int(input("Enter number:"))
+count=0
+while(n>0):
+    count=count+1
+    n=n//10
+print("The number of digits in the number are:",count)
