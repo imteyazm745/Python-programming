e7679cc437baec8360ff9501284d39100829cc47
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-12-14 02:07:26 +0530

    Create simple_calculator.py

---
+++ Basic Python/simple_calculator.py
@@ -0,0 +1,18 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+sum = a+b
+print("sum of {0} and {1} is {2}".format(a ,b, sum))
+sub = a-b
+print("Subtraction of {0} and {1} is {2}".format(a ,b, sub))
+mul = a*b
+print("Multiplication of {0} and {1} is {2}".format(a, b, mul))
+div = a/b
+print("Division of {0} and {1} is {2}".format(a, b, div))
+"""
+Enter first number : 3
+Enter second number : 2
+sum of 3 and 2 is 5
+Subtraction of 3 and 2 is 1
+Multiplication of 3 and 2 is 6
+Division of 3 and 2 is 1.5
+"""
