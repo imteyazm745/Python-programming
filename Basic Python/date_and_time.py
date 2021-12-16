b1c9ec87968869f046182f3ecbc1670b19753a2a
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-12-17 00:25:11 +0530

    Create date_and_time.py

---
+++ Basic Python/date_and_time.py
@@ -0,0 +1,9 @@
+import datetime
+now = datetime.datetime.now()
+print("Current date and Time is : ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+"""
+Current date and Time is : 
+2021-12-17 00:23:37
+"""
