2e685e93d68aadf7e5e1a85b0708b79646e4abdb
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-12-19 01:41:50 +0530

    Create simple_string1.py

---
+++ Udacity_Python/simple_string1.py
@@ -0,0 +1,5 @@
+cat_string = "--Whiskers--, --Spot--, --Meowmeow--, --Tiger--, --Kitty--, --Henry--, --Mr.Paws--"
+print(cat_string[16:20] + " is a good "+cat_string[51:56])
+"""
+Output:- Spot is a good Kitty
+"""
