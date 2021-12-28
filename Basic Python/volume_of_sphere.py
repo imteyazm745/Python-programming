0d52795e62dde6ae3066626bcf77b182f6cf1317
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2021-12-28 21:36:12 +0530

    Create volume_of_sphere.py

---
+++ Basic Python/volume_of_sphere.py
@@ -0,0 +1,10 @@
+# python program to calculate the volume of sphere 
+r = int(input("Enter radius of sphere : "))
+pi = 3.14159265358971931
+v = (4.0/3.0)*pi*r**3
+print("The volume of sphere is : ",v)
+"""
+output:-
+Enter radius of sphere : 6
+The volume of sphere is :  904.778684233839
+"""
