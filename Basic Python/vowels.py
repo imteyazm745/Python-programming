7b610bc19116c00c00ea8e982a39078267cb7cb0
Md Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-01-01 01:29:30 +0530

    Create vowels.py

---
+++ Basic Python/vowels.py
@@ -0,0 +1,14 @@
+# python program to test whether a passed letter is vowel or not
+def is_vowel(char):
+    all_vowels = 'aeiou'
+    return char in all_vowels
+print(is_vowel('c'))
+print(is_vowel('e'))
+print(is_vowel('t'))
+
+"""
+output:-
+False
+True
+False
+"""
