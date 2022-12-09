c365453777d2ec9cb4fa6833d4bd358d75fda369
Mohammad Imteyaz <52629525+imteyazm745@users.noreply.github.com>
2022-12-09 16:40:04 +0530

    Update star_pattern.py

--- Interview_preparations/star_pattern.py
+++ Interview_preparations/star_pattern.py
@@ -46,8 +46,18 @@ for i in range(n):
 *
 """
 
+# pyramid star pattern
+n = 5
+for i in range(1, n+1):
+    print(' ' * (n - i) + '*' * (2 * i - 1))
 
-
+"""
+    *
+   ***
+  *****
+ *******
+*********
+"""
 
 
 
