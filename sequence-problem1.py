47d7cd808f0df3f80d2a43ac16ac01badf8af5b7
imteyazm745 <imteyazm745@gmail.com>
2023-07-24 12:00:56 +0530

    moved in another file

--- sequence-problem1.py
+++
@@ -1,9 +0,0 @@
-# Slicing
-hello = 'Hello, world!'
- 
-a = hello[0]  # Turn me into 'H'
-b = hello[-2]  # Turn me into 'd'
-c = hello[1:5]  # Turn me into 'ello'
-d = hello[7:]  # Turn me into 'world!'
-e = hello[::2]  # Turn me into 'Hlo ol!'
-f = hello[::-1]  # Turn me into '!dlrow ,olleH'
