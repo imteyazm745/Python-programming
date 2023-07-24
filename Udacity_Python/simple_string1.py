a74db33a30517ea7f23d7d49905c625e36feb3e8
imteyazm745 <imteyazm745@gmail.com>
2023-07-24 11:57:13 +0530

    saved in another file

--- Udacity_Python/simple_string1.py
+++
@@ -1,5 +0,0 @@
-cat_string = "--Whiskers--, --Spot--, --Meowmeow--, --Tiger--, --Kitty--, --Henry--, --Mr.Paws--"
-print(cat_string[16:20] + " is a good "+cat_string[51:56])
-"""
-Output:- Spot is a good Kitty
-"""
