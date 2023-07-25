69ec2ad91c7ec9f3c101bc96283c48fcf0b6760f
imteyazm745 <imteyazm745@gmail.com>
2023-07-25 10:29:12 +0530

    modified

---
+++ Basic Python/roots_quad_eqn.py
@@ -0,0 +1,23 @@
+# Python program to find roots of quadratic equation 
+
+import math
+
+def find_roots(a, b, c):
+    
+    dis_form = b*b - 4*a*c
+    sqrt_val = math.sqrt(abs(dis_form))
+    
+    
+    if dis_form > 0:
+        print("Real and different roots")
+        print((-b+sqrt_val)/(2*a))
+        print((-b-sqrt_val)/(2*a))
+        
+    elif dis_form == 0:
+        print("Real and same roots")
+        print(-b/(2*a))
+        
+    else:
+        print("Complex Roots")
+        print(-b/(2*a),"+i", sqrt_val)
+        print(-b/(2*a),"-i", sqrt_val)
