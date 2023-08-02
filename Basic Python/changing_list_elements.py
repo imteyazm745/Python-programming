88e1817322fff214a70968bdfa919d954e5d059c
imteyazm745 <imteyazm745@gmail.com>
2023-08-02 11:52:41 +0530

    question

---
+++ Basic Python/changing_list_elements.py
@@ -0,0 +1,11 @@
+# Python program to interchange first and last elements in a list.
+
+"""Given a list, write a Python program to swap first and last element of the list.
+
+Examples: 
+
+Input : [12, 35, 9, 56, 24]
+Output : [24, 35, 9, 56, 12]
+
+Input : [1, 2, 3]
+Output : [3, 2, 1]"""
