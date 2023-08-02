709294ec49c5fbdabadbec7038231be776b1b074
imteyazm745 <imteyazm745@gmail.com>
2023-08-02 11:55:58 +0530

    first approach

--- Basic Python/changing_list_elements.py
+++ Basic Python/changing_list_elements.py
@@ -9,3 +9,16 @@ Output : [24, 35, 9, 56, 12]
 
 Input : [1, 2, 3]
 Output : [3, 2, 1]"""
+
+# Approach 1: swap first and last elements
+def swapList(list):
+    size = len(list)
+    
+    temp = list[0]
+    list[0] = list[size-1]
+    list[size-1] = temp
+    
+    return list
+
+list = [15, 40, 5, 97, 21]
+print(swapList(list))
